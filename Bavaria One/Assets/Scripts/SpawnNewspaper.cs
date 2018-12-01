af1e736c281375f0ee4bf2536634b47e7a82e812
MININT-G1JCQLG\lisak <lisa.kaldich@googlemail.com>
2018-12-01 18:42:08 +0100

    Spin mehrerer Zeitungen

---
+++ Bavaria One/Assets/Scripts/SpawnNewspaper.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SpawnNewspaper : MonoBehaviour {
+    
+    public Sprite[] images;
+    public GameObject NewspaperPrefab;
+    public Transform parent;
+    int i;
+    GameObject currentNewspaper;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space) )
+        {
+            InstantiateNewspaper();
+        }
+       
+        if (Input.GetMouseButton(0))
+        {
+            currentNewspaper.GetComponent<Animator>().SetTrigger("fadeout");
+        }
+    }
+
+    public void InstantiateNewspaper() {
+        if (i < images.Length)
+        {
+            currentNewspaper = Instantiate(NewspaperPrefab, parent);
+            currentNewspaper.GetComponent<Image>().sprite = images[i];
+            i++;
+        }
+    }
+}
