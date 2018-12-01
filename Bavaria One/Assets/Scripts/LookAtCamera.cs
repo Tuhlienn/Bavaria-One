9831bc048da06cdbdb2a63d6516a5063be433c1a
pomelova <anastasiapome@gmail.com>
2018-12-01 21:42:09 +0100

    LookAtCamera.cs

---
+++ Bavaria One/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour {
+
+    private GameObject cam;
+
+	// Use this for initialization
+	void Start () {
+
+        cam = GameObject.Find("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.LookAt(cam.transform);
+	}
+}
