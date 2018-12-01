486ca8b5f28f43b1b028cfd2c423b2bdf0f8bcd4
pomelova <anastasiapome@gmail.com>
2018-12-01 13:43:10 +0100

    Menu setup

---
+++ Bavaria One/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+
+
+    public void OnToggleBuildMode()
+    {
+        //gamemanager.buildmode = !buildmode
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void UpdateResources()
+    {
+
+    }
+}
