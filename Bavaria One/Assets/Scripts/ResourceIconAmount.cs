1720d345a00b9c976d23ba339d8fbe91fcde3891
Quwertzuiopp <demian.joerden@tum.de>
2018-12-02 12:28:45 +0100

    fh

--- Bavaria One/Assets/Scripts/ResourceIconAmount.cs
+++ Bavaria One/Assets/Scripts/ResourceIconAmount.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class ResourceIconAmountScript: MonoBehaviour {
+public class ResourceIconAmount: MonoBehaviour {
     public float CurrentAmount;
 	// Use this for initialization
 	void Start () {
