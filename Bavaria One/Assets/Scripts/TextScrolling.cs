937f9e3042cb88bf50f6af5f29db5e1e71de4d7a
MININT-G1JCQLG\lisak <lisa.kaldich@googlemail.com>
2018-12-02 10:17:19 +0100

    added Mars to Intro

--- Bavaria One/Assets/Scripts/TextScrolling.cs
+++ Bavaria One/Assets/Scripts/TextScrolling.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 public class TextScrolling : MonoBehaviour {
 
     public float scrollSpeed = 20;
-	
-	// Update is called once per frame
-	void Update () {
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        StartCoroutine(MyCoroutine());
+
         Vector3 pos = transform.position;
-        Vector3 localVectorUp = transform.TransformDirection(0, 1, 0);
-        pos += localVectorUp * scrollSpeed * Time.deltaTime;
-        transform.position = pos;
-	}
+        if (transform.position.y < 250.0f)
+        {
+            Vector3 localVectorUp = transform.TransformDirection(0, 1, 0);
+            pos += localVectorUp * scrollSpeed * Time.deltaTime;
+            transform.position = pos;
+        }
+        else
+        {
+            //change Scene?
+        }
+    }
+
+    IEnumerator MyCoroutine()
+    {
+        yield return new WaitForSeconds(27);
+    }
 }
