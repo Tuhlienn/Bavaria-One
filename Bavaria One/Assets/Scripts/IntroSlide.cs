b84311f7524c66279323cbe05fdeddfe37898484
MININT-G1JCQLG\lisak <lisa.kaldich@googlemail.com>
2018-12-02 08:49:32 +0100

    added intro

---
+++ Bavaria One/Assets/Scripts/IntroSlide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class IntroSlide : MonoBehaviour {
+
+    public Image introText;
+    float moveRate = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+        introText = GetComponent<Image>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (introText.transform.position.y > 350) {
+          moveRate = 0.0f;
+        introText.color = new Vector4(0, 0, 0, 0);
+        }
+        introText.transform.localPosition += new Vector3(0, moveRate, moveRate);
+    }
+}
---
+++ Bavaria One/Assets/Scripts/LogoSlide.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LogoSlide : MonoBehaviour {
+
+    public Image image;
+    float scaleRate = -0.01f;
+    float minScale= 0.8f;
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (image.transform.localScale.x < minScale)
+        {
+            scaleRate = 0.0f;
+            image.color = new Vector4(0, 0, 0, 0);
+        }
+        image.transform.localScale += Vector3.one * scaleRate;
+    }   
+}
