521a04461fea20f2889bd1494e909d5150d9894a
Dudadei <markus.klaus@tum.de>
2018-12-02 12:19:12 +0100

    uiuiuiuiuiuiuiuiui

--- Bavaria One/Assets/Scripts/UI/ButtonManager.cs
+++ Bavaria One/Assets/Scripts/UI/ButtonManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEditor;
+using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour {
 
@@ -152,6 +153,11 @@ public class ButtonManager : MonoBehaviour {
     public void ToggleCredits()
     {
         creditsObject.SetActive(!creditsObject.active);
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
     }
 
 }
